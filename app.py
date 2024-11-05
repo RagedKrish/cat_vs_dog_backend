9cd856457bbc66b0b9359134d8b2c5d480352cd6
Krishs21 <117849849+RagedKrish@users.noreply.github.com>
2024-11-06 00:21:43 +0530

    Add main app files and model

---
+++ app.py
@@ -0,0 +1,57 @@
+# app.py
+from flask import Flask, request, jsonify
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array, load_img
+import numpy as np
+import os
+from werkzeug.utils import secure_filename
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins=['https://myfrontend.com'])
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0', debug=False)
+
+model = load_model('best_cat_dog_classifier.keras')  # Load the pre-trained model
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+def preprocess_image(image_path):
+    img = load_img(image_path, target_size=(128, 128))  # Adjust for model input size
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)  # Add batch dimension
+    img /= 255.0
+    return img
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file provided'}), 400
+
+    file = request.files['file']
+    if file:
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        img = preprocess_image(file_path)
+        prediction = model.predict(img)
+        confidence_threshold = 0.5
+
+        if prediction >= confidence_threshold:
+            result = "dog"
+            confidence = (float(prediction))*100
+        else:
+            result = "cat"
+            confidence = (float(1-prediction))*100
+
+        try:
+            os.remove(file_path)
+        except OSError as e:
+            print(f"Error deleting file {file_path}: {e}")
+
+        return jsonify({'result': result, 'confidence': round(confidence, 2)})
+    return jsonify({'error': 'Failed to process the image'}), 400
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
